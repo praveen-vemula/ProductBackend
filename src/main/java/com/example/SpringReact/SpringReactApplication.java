f4e592469297ea42c8b6e502dfda67f60bdc011a
praveen-vemula <155568664+praveen-vemula@users.noreply.github.com>
2024-01-18 12:00:12 -0600

    Testing

--- src/main/java/com/example/SpringReact/SpringReactApplication.java
+++ src/main/java/com/example/SpringReact/SpringReactApplication.java
@@ -8,6 +8,7 @@ public class SpringReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactApplication.class, args);
+		System.out.println("Testing")
 	}
 
 }
