41a94d74d5240bce3796867965e6c8efad20f3f5
praveen-vemula <155568664+praveen-vemula@users.noreply.github.com>
2024-01-18 12:11:32 -0600

    code changes

--- src/main/java/com/example/SpringReact/SpringReactApplication.java
+++ src/main/java/com/example/SpringReact/SpringReactApplication.java
@@ -8,7 +8,7 @@ public class SpringReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactApplication.class, args);
-		System.out.println("Testing")
+		System.out.println("Testing");
 	}
 
 }
